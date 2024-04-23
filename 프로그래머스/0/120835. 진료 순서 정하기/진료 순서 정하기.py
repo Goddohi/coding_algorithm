9ff1c5de6f99fab74673c4070c3b708cd552607f
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-04-23 23:32:47 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,9 @@
+def solution(emergency):
+    arr = []
+    for i in emergency:
+        idx = 1
+        for j in emergency:
+            if i < j:
+                idx += 1
+        arr.append(idx)
+    return arr
