ff702f5a3c6ceb38c38e655750553bb71d59255b
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-04-30 15:45:48 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.57 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,25 +1,15 @@
-    
-                           
-                           
 def solution(number, limit, power):
-    #number가 1일경우는 연산제외 limit가 2이상이라 연산없이 계산가능
-    answer = 1
-    setting=[]
-    #number가 1일경우는 연산제외
-    if(number>1):
-        for semiNumber in range(2, number+1):
-            settemp=set({1,semiNumber}) 
-            for i in range(1,int((semiNumber**(1/2)+1))):
-                if(semiNumber==i**2):
-                    settemp.add(i)
-                    break
-                elif(semiNumber%i==0):
-                    settemp.add(i)
-                    settemp.add(semiNumber//i)
-                if( len(settemp)>limit):
-                    break
-            if(limit<len(settemp)):
-                answer+=power
-            else:
-                answer+=len(settemp)
+    answer = 0
+    for semiNumber in range(1, number+1):
+        settemp=set()
+        for i in range(1,int((semiNumber**(1/2)+1))):
+            if(semiNumber%i==0):
+                settemp.add(i)
+                settemp.add(semiNumber//i)
+            if(len(settemp)>limit):
+                break
+        if(limit<len(settemp)):
+            answer+=power
+        else:
+            answer+=len(settemp)
     return answer
