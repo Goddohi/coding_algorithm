24e845c0f875169b67134368816a35d61444a95f
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-05-08 13:33:47 +0900

    Update 폰켓몬.java

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         HashSet<Integer> hashSet = new HashSet<>();
-            for(int i =0; i<nums.length;i++) {
+            for(int i =0; i<nums.length;i++) 
                 hashSet.add(nums[i]);
-            }
-            return hashSet.size()>nums.length/2 ? nums.length/2 :hashSet.size();
+            
+        return hashSet.size()>nums.length/2 ? nums.length/2 :hashSet.size();
     }
 }
