65e73d577efd558b8049050cb626987b054431b0
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-05-08 13:29:49 +0900

    [level 1] Title: 폰켓몬, Time: 2.55 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         HashSet<Integer> hashSet = new HashSet<>();
             for(int i =0; i<nums.length;i++) {
                 hashSet.add(nums[i]);
             }
-            if(hashSet.size()>nums.length/2)
-                return nums.length/2;
-            return hashSet.size();
+            return hashSet.size()>nums.length/2 ? nums.length/2 :hashSet.size();
     }
 }
