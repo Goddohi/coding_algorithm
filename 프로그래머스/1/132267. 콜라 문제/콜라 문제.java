af9b74181fc70f5eebdbd182434e351431c3cf1e
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-05-08 10:51:04 +0900

    콜라문제 추가 설명

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
         while(n>=a){
-            answer += b*((n-(n%a))/a);
-            n=b*((n-(n%a))/a)+n%a;
+            answer += b*((n-(n%a))/a); //answer += b*(n/a); 해도 괜찮다
+            n=b*((n-(n%a))/a)+n%a; //n=b*(n/a)+n%a; 해도 괜찮다
         }
          
         return answer;
