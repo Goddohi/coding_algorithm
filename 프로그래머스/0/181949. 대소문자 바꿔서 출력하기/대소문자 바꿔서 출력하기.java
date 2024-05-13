ba92127635a6ac0917a540bfb9af5c0460e7e0d0
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-05-13 16:31:01 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 227.96 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -7,9 +7,9 @@ public class Solution {
         
         for(int i=0;i<a.length();i++){
             int temp = (int)a.charAt(i);
-            if(temp>=65 & temp<=90)
+            if(temp>=65 && temp<=90)
                 temp +=32;
-            else if(temp>=97 & temp<=122)
+            else if(temp>=97 && temp<=122)
                 temp -=32;
             
             System.out.print((char)temp);
