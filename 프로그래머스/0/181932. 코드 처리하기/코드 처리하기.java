b31b0db42a7a2ba4d76f019175d560f907de62f3
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-05-27 02:08:54 +0900

    [level 0] Title: 코드 처리하기, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder answer = new StringBuilder();
+        int mode=0;
+        for(int i = 0; i<code.length();i++){
+            if(code.charAt(i) =='1'){
+                mode = mode==0 ? 1:0;
+                continue;
+            }
+            if(i%2 ==mode){
+                answer.append(code.charAt(i));   
+            }
+        }
+        return answer.toString().length()!=0 ? 
+               answer.toString() : "EMPTY";
+    }
+}
