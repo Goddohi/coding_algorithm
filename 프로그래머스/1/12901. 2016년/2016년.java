aab9849c52ceef0b5ea7dd4842925d3c53da1f7d
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-06-06 23:37:35 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(int a, int b) {
+        int[] mont ={0,31,29,31,30,31,30,31,31,30,31,30,31};
+        String answer = "";
+        for(int i=0;i<a;i++){
+            b+=mont[i];
+        }
+        switch(b%7){
+            case 1: answer = "FRI"; break;
+            case 2: answer = "SAT"; break;
+            case 3: answer = "SUN"; break;
+            case 4: answer = "MON"; break;
+            case 5: answer = "TUE"; break;
+            case 6: answer = "WED"; break;
+            case 0: answer = "THU"; break;
+            default:               
+        }
+        
+        return answer;
+    }
+}
