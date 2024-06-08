85051e69fade1dd8da3d7fbe53b042a0935593d8
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-06-08 17:22:54 +0900

    [level 0] Title: 배열 만들기 2, Time: 8.41 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        ArrayList<Integer> answer_list = new ArrayList<Integer>();
+       for(int i = l; i < r + 1; i++) {
+           if(i%5 != 0){
+               continue;
+           }
+           String strNum = String.valueOf(i);
+           boolean count = true;
+           for(int j = 0; j < strNum.length(); j++) {
+                if(strNum.charAt(j) != '0' && strNum.charAt(j) != '5') {
+                    count= false;
+                }
+            }
+           if(count ==true)
+               answer_list.add(i);
+        }
+        return list_array(answer_list);
+    }
+      
+ 
+    
+    public int[] list_array(ArrayList<Integer> list){
+        if(list.size()==0){
+            int[] i = {-1};
+            return i;
+        }
+        int[] i = new int[list.size()];
+        for(int k=0;k<list.size();k++){
+            i[k]=list.get(k);
+        }
+        return i;
+    }
+}
