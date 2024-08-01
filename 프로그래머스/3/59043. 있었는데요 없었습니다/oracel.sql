a8d45690fdd63cbb7806a2e4c5e73ad371b9c27c
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-08-01 23:37:01 +0900

    mysql&oracel.sql

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/oracel.sql
@@ -0,0 +1,9 @@
+SELECT    i.ANIMAL_ID   as ANIMAL_ID 
+        , i.NAME        as NAME 
+  FROM    ANIMAL_INS i 
+        , ANIMAL_OUTS o
+ 
+ WHERE i.ANIMAL_ID = o.ANIMAL_ID
+   AND i.DATETIME  > o.DATETIME
+ 
+ ORDER BY i.DATETIME ASC
