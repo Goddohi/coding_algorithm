352d5e5d27959e942a40fdf3bda4d79b32116353
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-08-03 04:00:34 +0900

    [level 0] Title: 배열 만들기 6, Time: 36.30 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,29 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> stk = new ArrayList<>();
-        for(int i = 0 ; i <arr.length;i++){ // 주어진 조건은 for문을 의미한다.
-            int size = stk.size();
-            if(size==0){
-                stk.add(arr[i]);
-            }else if(stk.get(size-1)==arr[i]){
-                stk.remove(size-1);
-            }else if(stk.get(size-1)!=arr[i]){  //else해도 괜찮다.
-                stk.add(arr[i]);           
-            }
-        }
-        
-        if(stk.size() == 0){
-            int[] answer = {-1}; 
-            return answer;
-        }
-        
-        int[] answer = new int[stk.size()];
-        for(int i = 0 ; i <answer.length;i++){
-            answer[i]=stk.get(i);
-        }
-        
-        return answer;     
+        int[] answer = {};
+        return answer;
     }
 }
