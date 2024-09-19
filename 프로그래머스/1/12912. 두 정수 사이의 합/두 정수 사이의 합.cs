f5f789b35caebb9ff1fb2286e48097496ff61e65
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-09-19 23:24:13 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 17.33 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public long solution(int a, int b) {
+        long answer = b;
+        while(a!=b){
+            answer +=a;
+            a =  a>b ? a-1: a+1;  
+        }
+        return answer;
+    }
+}
