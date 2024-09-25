efa1edcaaa6180976f94cd99932903c70a7d4f9d
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-09-26 01:11:37 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(long num) {
+        for(int i=0 ; i<500;i++){
+            if(num==1){
+                return i;
+            }
+            
+            num = num%2==0 ? num / 2 :num*3 +1;
+          
+        }
+        if(num==1){
+            return 500;
+        }
+        return -1;
+    }
+}
