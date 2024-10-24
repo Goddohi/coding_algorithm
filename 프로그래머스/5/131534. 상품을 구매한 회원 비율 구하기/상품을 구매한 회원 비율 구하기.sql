6d8b3c62d4e20606689d4dfabb3f88bcb409d8b0
최경태 <129163714+Goddohi@users.noreply.github.com>
2024-10-24 22:44:46 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,15 +1,19 @@
-SELECT       TO_CHAR(o.SALES_DATE,'YYYY') AS year 
-           , TO_NUMBER(TO_CHAR(o.SALES_DATE,'MM')) AS month 
-           , COUNT(DISTINCT o.user_id) as puchased_users
-           , ROUND(COUNT(DISTINCT o.user_id)/(SELECT count(user_id)
-                                                FROM user_info 
-                                               WHERE TO_CHAR(joined,'YYYY')  = '2021'),1) as puchased_ratio
-    
-  FROM       ONLINE_SALE o , (SELECT user_id 
-                                FROM user_info 
-                               WHERE TO_CHAR(joined,'YYYY')  = '2021') u
- 
- WHERE    u.user_id = o.user_id
- GROUP BY TO_CHAR(o.SALES_DATE,'YYYY'), TO_NUMBER(TO_CHAR(o.SALES_DATE,'MM')) 
- ORDER BY year, month
-;
+WITH ALL_MEMBER AS (
+    SELECT * FROM
+        USER_INFO
+    WHERE JOINED LIKE '2021%'
+)
+
+SELECT YEAR(OS.SALES_DATE) AS YEAR, 
+       MONTH(OS.SALES_DATE) AS MONTH, 
+       COUNT(DISTINCT OS.USER_ID) AS PURCHASED_USERS,
+       ROUND(COUNT(DISTINCT OS.USER_ID) / (SELECT COUNT(*) FROM ALL_MEMBER), 1)  AS PUCHASED_RATIO
+FROM 
+USER_INFO UI
+JOIN
+    ONLINE_SALE OS
+ON
+    UI.USER_ID = OS.USER_ID      
+WHERE JOINED LIKE '2021%'
+GROUP BY 1, 2
+ORDER BY 1, 2
